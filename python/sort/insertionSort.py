455c3d252627b716c9bea4a7d74032bfed34c64b
yxfcodeup <825378343@qq.com>
2017-03-24 17:28:24 +0800

    update

---
+++ python/sort/insertionSort.py
@@ -0,0 +1,11 @@
+import os
+
+
+
+def insertionSort(target) :
+    _len = len(target)
+    for i in range(_len) :
+        _key = target[i]
+        j = i - 1
+        while j > 0 and target[j]  > key :
+
